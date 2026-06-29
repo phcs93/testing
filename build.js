3f170a369528f5b80ab7efe2d362b21f36e53309
phcs93 <phcs93@gmail.com>
2026-06-29 18:22:29 -0300

    testing

---
+++ build.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+
+const langs = ["en", "pt"];
+
+function CreateBinFolder() {
+
+    if (fs.existsSync("bin")) {
+        fs.rmSync("bin", { recursive: true, force: true });
+    }
+
+    fs.mkdirSync("bin");
+
+}
+
+function CopyCommonFiles() {
+
+    fs.cpSync(`lib`, `bin/$common/lib`, { recursive: true });
+    fs.cpSync(`res`, `bin/$common/res`, { recursive: true });
+    fs.cpSync(`src`, `bin/$common/src`, { recursive: true });
+
+    // we will move the pages to each corresponding lang folder
+    fs.rmSync(`bin/$common/src/pages`, { recursive: true, force: true });
+
+}
+
+// function CreateLangFolders() {
+
+//     for (const lang of langs) {
+//         const langFolder = `bin/${lang}`;
+//         fs.mkdirSync(langFolder);
+//     }
+
+// }
+
+function CopyPagesToLangFolders() {
+
+    function copyPages(src, dst, lang) {
+
+        function walk(currentSrc) {
+
+            const entries = fs.readdirSync(currentSrc, { withFileTypes: true });
+
+            for (const entry of entries) {
+
+                const srcFile = currentSrc + "/" + entry.name;
+
+                if (entry.isDirectory()) {
+                    walk(srcFile);
+                    continue;
+                }
+
+                const relative = srcFile.replace("{lang}", lang).substring(src.length + 1);
+
+                const dot = relative.lastIndexOf(".");
+                const ext = relative.substring(dot);
+                const withoutExt = relative.substring(0, dot);
+
+                if (!withoutExt.endsWith("index")) {
+                    const dstFile = dst + "/" + withoutExt + "/index" + ext;                
+                    const dir = dstFile.substring(0, dstFile.lastIndexOf("/"));
+                    fs.mkdirSync(dir, { recursive: true });
+                    fs.copyFileSync(srcFile, dstFile);
+                } else {
+                    const dstFile = dst + "/" + withoutExt + ext;
+                    fs.copyFileSync(srcFile, dstFile);
+                }
+
+            }
+
+        }
+
+        walk(src);
+
+    }
+
+    for (const lang of langs) {
+        copyPages(`src/pages`, "bin", lang);
+    }
+
+}
+
+function FilterLangTags() {
+
+    const filter = (html, lang) => {
+        return html
+            .replace(new RegExp(`<([a-z0-9]+)\\b[^>]*\\blang="(?!${lang}")[^"]+"[^>]*>[\\s\\S]*?<\\/\\1>`, 'gi'), '')
+            .replace(new RegExp(`\\s+lang="${lang}"`, 'gi'), '')
+        ;
+    };
+
+    const walk = (currentSrc, lang) => {
+        for (const entry of fs.readdirSync(currentSrc, { withFileTypes: true })) {
+            if (entry.isDirectory()) {
+                walk(currentSrc + "/" + entry.name, lang);
+            } else {
+                const srcFile = currentSrc + "/" + entry.name;
+                const html = fs.readFileSync(srcFile, "utf-8");
+                const filteredHtml = filter(html, lang);
+                fs.writeFileSync(srcFile, filteredHtml, "utf-8");
+            }
+        }
+    }
+
+    for (const lang of langs) {
+        walk(`bin/${lang}`, lang);
+    }
+
+}
+
+CreateBinFolder();
+CopyCommonFiles();
+// CreateLangFolders();
+CopyPagesToLangFolders();
+FilterLangTags();
